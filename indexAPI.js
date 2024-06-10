3ad0082e6d949eadb1ebb62938e755372cb8a3df
DR-Dev-Jack <114923262+DR-Dev-Jack@users.noreply.github.com>
2024-06-10 11:10:52 +0200

    bugtesting 🐞

--- indexAPI.js
+++ indexAPI.js
@@ -1,32 +1,21 @@
 var currentLatitude;
 var currentLongitude;
 
-async function getData(api_url) {
+async function getData(api_url) { // leest antwoord in en wacht voor json response
     const response = await fetch(api_url);
     const data = await response.json();
     return new Promise(resolve => setTimeout(() => resolve(data), 100));
 }
 
-function getLocation() {
+function getLocation() { // maakt een geolocation aan
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(requestWeather);
+        navigator.geolocation.getCurrentPosition(extractCoords);
     } else { 
         console.log("4, not working");
     }
 }
 
-async function requestWeather(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-    console.log(longitude, latitude);
-
-    const baseUrlWeather = "https://api.weatherapi.com/v1/forecast.json";
-    const weatherKey = "a5e5b58ca02f405c915110838240406";
-    const weatherRest = "&days=1&aqi=no&alerts=no";
-    const weatherRequest = baseUrlWeather + "?key=" + weatherKey + "&q=" + latitude + "," + longitude + weatherRest;
-
-    const jsonWeatherReply = await getData(weatherRequest);
-    
+function WriteWeatherVariablesToText(jsonWeatherReply) { // maakt json variables en schrijft ze naar de html
     const avgHumidity = jsonWeatherReply["forecast"]["forecastday"][0]["day"]["avghumidity"];
     const avgTemp = jsonWeatherReply["forecast"]["forecastday"][0]["day"]["avgtemp_c"];
     const rain_chance = jsonWeatherReply["forecast"]["forecastday"][0]["day"]["daily_chance_of_rain"];
@@ -35,7 +24,6 @@ async function requestWeather(position) {
     const min_temp = jsonWeatherReply["forecast"]["forecastday"][0]["day"]["mintemp_c"];
     const rain = jsonWeatherReply["forecast"]["forecastday"][0]["day"]["totalprecip_mm"];
 
-    console.log(rain_chance)
     document.getElementById("textRainChance").innerHTML = rain_chance.toString();
     document.getElementById("textRainFall").innerHTML = rain.toString();
     document.getElementById("textWindSpeed").innerHTML = max_wind.toString();
@@ -43,4 +31,55 @@ async function requestWeather(position) {
     document.getElementById("textMaxTemp").innerHTML = max_temp.toString();
 }
 
-getLocation();
+async function extractCoords(position) { // gebruikt de geolocation en getData functie voor waarden en meer
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+
+
+    const baseUrlWeather = "https://api.weatherapi.com/v1/forecast.json";
+    const weatherKey = "a5e5b58ca02f405c915110838240406";
+    const weatherRest = "&days=1&aqi=no&alerts=no";
+    const weatherRequest = baseUrlWeather + "?key=" + weatherKey + "&q=" + latitude + "," + longitude + weatherRest;
+
+    jsonWeatherReply = await getData(weatherRequest);
+    
+    setCookie("jsonWeatherReply", jsonWeatherReply) // hier nog een datum
+    WriteWeatherVariablesToText(jsonWeatherReply)    
+}
+
+function setCookie(cname, cvalue) { // maakt een cookie aan voor een dag
+    const now = new Date();
+    const endOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+    console.log(endOfDay)
+    const expires = "expires=" + endOfDay.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+
+function getCookie(cname) { // zoekt mijn cookies
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+
+jsonWeatherReply = getCookie("jsonWeatherReply")
+if (jsonWeatherReply != "") {
+    console.log("uses cookie");
+    console.log("returned cookie:", jsonWeatherReply)
+    WriteWeatherVariablesToText(jsonWeatherReply);
+} else {
+    console.log("uses api");
+    getLocation();
+    console.log("returned api:", jsonWeatherReply)
+}
